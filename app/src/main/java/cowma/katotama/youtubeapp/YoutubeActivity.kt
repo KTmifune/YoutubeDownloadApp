ac608efb6157ab44cd61fc005dacd992288dc713
Takuma Kato <k.t.mifune@gmail.com>
2020-07-04 23:55:33 +0900

    no message

---
+++ app/src/main/java/cowma/katotama/youtubeapp/YoutubeActivity.kt
@@ -0,0 +1,12 @@
+package cowma.katotama.youtubeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class YoutubeActivity : YoutubeActivity {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube)
+    }
+}
